3a2d18353c7e2204ed9b8bc417d22db1c72a4638
progchris00 <prog.christian07@gmail.com>
2024-05-17 17:47:42 +0800

    add: implement model diagnosis

---
+++ assets/script/index.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  var image = $("img");
+  var selectedItems = [];
+  $("#selections").html(
+    selectedItems.length > 0
+      ? "<b>Selected body Parts: </b>" + selectedItems
+      : "<b>Please select a body part</b>"
+  );
+
+  var defaultDipTooltip = "<b><u>Spine</u></b>";
+
+  image.mapster({
+    fillOpacity: 0.4,
+    fillColor: "643dff",
+    strokeColor: "643dff",
+    strokeOpacity: 0.8,
+    strokeWidth: 2,
+    stroke: true,
+    isSelectable: true,
+    singleSelect: false,
+    mapKey: "name",
+    listKey: "key",
+    onClick: function (e) {
+      var newToolTip = defaultDipTooltip;
+      if ($.inArray(e.key, selectedItems) >= 0) {
+        selectedItems.splice($.inArray(e.key, selectedItems), 1);
+      } else {
+        selectedItems.push(e.key);
+      }
+      // $('#selections').html( selectedItems.length);
+      $("#selections").html(
+        selectedItems.length > 0
+          ? "<b>Selected body Parts: </b>" +
+              selectedItems
+                .toString()
+                .replace(new RegExp("_", "g"), " ")
+                .replace(new RegExp(",", "g"), ", ")
+          : "<b>Please select a body part</b>"
+      );
+    },
+    showToolTip: true,
+    toolTipClose: ["tooltip-click", "area-click"],
+    areas: [
+      {
+        name: "12",
+        key: "12",
+        selected: true,
+        strokeColor: "FFFFFF",
+      },
+    ],
+  });
+});
