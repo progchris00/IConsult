77c0ea372e96c41918a90ef843c16619bb64ebcf
progchris00 <prog.christian07@gmail.com>
2024-05-21 11:18:03 +0800

    organize file using flask templating
    Used jinja to make templates of html pages. Moved files from assets folder to static to enable loading on flask template. Deleted files no longer needer

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/diagnosis")
+def diagnosis():
+    return render_template('diagnosis.html')
+
+@app.route("/assessment")
+def assessment():
+    return render_template('assessment.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
